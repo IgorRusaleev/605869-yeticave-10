c7b13777c20330bd4721b40d6d505bef068f9aee
Igor Rusaleev <igor.rusaleev@list.ru>
2019-08-20 18:50:57 +0300

    Создание таблицы категорий

---
+++ queries.sql
@@ -0,0 +1,13 @@
+/*Создание таблицы категорий*/
+INSERT INTO category
+SET name_cat = 'Доски и лыжи', character_code = 'boards';
+INSERT INTO category
+SET name_cat = 'Крепления', character_code = 'attachment';
+INSERT INTO category
+SET name_cat = 'Ботинки', character_code = 'boards';
+INSERT INTO category
+SET name_cat = 'Одежда', character_code = 'clothing';
+INSERT INTO category
+SET name_cat = 'Инструменты', character_code = 'tools';
+INSERT INTO category
+SET name_cat = 'Разное', character_code = 'other';
