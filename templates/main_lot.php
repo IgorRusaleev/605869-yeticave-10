59bfa726170ca88e352e02c3db23b18cc135be92
Igor Rusaleev <igor.rusaleev@list.ru>
2019-08-28 16:52:07 +0300

    Работа со строкой запроса, создание страниц лотов

--- templates/main_lot.php
+++ templates/main_lot.php
@@ -30,7 +30,7 @@
                     <p class="lot-item__form-item form__item form__item--invalid">
                         <label for="cost">Ваша ставка</label>
                         <input id="cost" type="text" name="cost" placeholder="<?=adding_ruble($lot[0]['step_rate']);?>">
-                        <span class="form__error">Введите наименование лота</span>
+                        <!--<span class="form__error">Введите наименование лота</span>-->
                     </p>
                     <button type="submit" class="button">Сделать ставку</button>
                 </form>
